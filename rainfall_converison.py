1022128220b1ee6d8c5cd8ff3fdec75d13a5a950
timothynunn <76562978+timothynunn@users.noreply.github.com>
2022-05-20 10:10:24 +0100

    Initial commit

---
+++ rainfall_converison.py
@@ -0,0 +1,15 @@
+"""A library to perform rainfall unit conversions"""
+
+def inches_to_mm(inches):
+    """
+    Convert inches to millimetres
+
+    Arguments:
+    inches -- the rainfall inches
+    """
+    mm = inches * 25.4
+    return mm
+
+# TODO: Add function inches_to_m
+# TODO: Add function inches_to_cm
+
